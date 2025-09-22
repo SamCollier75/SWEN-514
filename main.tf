b6b9c2a674ca8627b4b84a0c9c04a876ba514d5b
SamCollier75 <111803721+SamCollier75@users.noreply.github.com>
2025-09-22 16:31:41 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,52 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"  # Set AWS region to US East 2 (Ohio)
+}
+
+# Local variables block for configuration values
+locals {
+    aws_key = "This_is_a_key"   # SSH key pair name for EC2 instance access
+}
+
+# EC2 instance resource definition
+resource "aws_instance" "my_server" {
+  ami           = data.aws_ami.amazonlinux.id  # Use the AMI ID from the data source
+  instance_type = var.instance_type            # Use the instance type from variables
+  key_name      = "This_is_a_key"          # Specify the SSH key pair name
+  
+  vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
+
+  user_data = file("wp_install.sh")
+
+  # Add tags to the EC2 instance for identification
+  tags = {
+    Name = "my ec2"
+  }                  
+}
+
+resource "aws_security_group" "allow_http_ssh" {
+  name        = "allow_http_ssh"
+  description = "Allow HTTP and SSH inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+}
---
+++ output.tf
@@ -0,0 +1,11 @@
+# Output to display the private IP addresses of all created EC2 instances
+# The [*] syntax allows for multiple instances if count or for_each is used
+output "instance_ip_addr" {
+  value = aws_instance.my_server[*].private_ip
+}
+
+# Output to display the public IP addresses of all created EC2 instances
+# The [*] syntax allows for multiple instances if count or for_each is used
+output "public_ip" {
+  value = aws_instance.my_server[*].public_ip
+}
